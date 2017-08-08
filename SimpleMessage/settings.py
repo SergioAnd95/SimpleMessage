7eec7b514593bec754bd37623f25b5aad94d332c
SergioAnd95 <developinc@yadnex.ru>
2017-08-08 16:29:12 +0300

    update settings

--- SimpleMessage/settings.py
+++ SimpleMessage/settings.py
@@ -142,7 +142,6 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Media files
 
